44d8f1b4b81ed618039b01d7330d32ea480da7a8
Lax Mariappan <elaxmariappan@gmail.com>
2023-01-27 20:57:14 +0530

    Added plugin file and plugin header

---
+++ at-a-glance-widget-plus.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+ * Plugin Name:       At A Glance Widget Plus
+ * Plugin URI:        https://github.com/laxmariappan/at-a-glance-widget-plus
+ * Description:       A WordPress plugin to extend the core at a glance widget.
+ * Version:           0.1.0
+ * Requires at least: 6.1
+ * Requires PHP:      8.1
+ * Author:            Lax Mariappan
+ * Author URI:        https://laxmariappan.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:        FALSE
+ * Text Domain:       at-a-glance-widget-plus
+ * Domain Path:       /languages
+ */
